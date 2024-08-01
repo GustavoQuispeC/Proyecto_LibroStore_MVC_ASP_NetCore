1901df9cd379ff7e3a2ffb3748b97a6cf73f3dac
Gustavo <gusstavocta@gmail.com>
2024-07-31 23:50:57 -0500

    implementando controller

--- src/AppStore/Controllers/HomeController.cs
+++ src/AppStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using AppStore.Repositories.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +12,17 @@ namespace AppStore.Controllers
     
     public class HomeController : Controller
     {
-       
-        public IActionResult Index()
+        private readonly ILibroService _libroService;
+
+        public HomeController(ILibroService libroService)
         {
-            return View();
+            _libroService = libroService;
+        }
+
+        public IActionResult Index(string term ="", int currentPage = 1)
+        {
+            var libros = _libroService.List(term, true, currentPage);
+            return View(libros);
         }
 
     }
